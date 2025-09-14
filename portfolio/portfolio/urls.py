9090db74e29b05cc56fdc87173ce29ae2bd605e1
Harshith9Aileneni <aileneni.h@northeastern.edu>
2025-09-13 21:47:44 -0700

    temp

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -1,9 +1,9 @@
-
+from django.contrib import admin
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('skills/', views.skills, name='skills'),
     path('contact/', views.contact_form, name='contact'),
